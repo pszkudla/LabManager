09f2cab87a4082ed618b509d7ca59e8ebd52ed0a
pszkudla <pszkudla@visa.com>
2026-08-28 14:30:28 +0200

    Added constraints to the created table to prevent crating duplicated in the substances table and set the proper length for the iupac_name property..

--- src/main/java/pl/visa/labmanager/substance/Substance.java
+++ src/main/java/pl/visa/labmanager/substance/Substance.java
@@ -1,6 +1,7 @@
 package pl.visa.labmanager.substance;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,11 +10,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name="substances")
+@Table(name="substances", uniqueConstraints = {@UniqueConstraint(columnNames = {"iupac_name", "cas_number"})})
 public class Substance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min=2, max=10240)
     private String iupacName;
     private String casNumber;
     private String smiles;
