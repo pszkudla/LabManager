574a5e6e55334aa83ed729eeac5aa2f651fa4d09
pszkudla <pszkudla@visa.com>
2026-08-31 08:42:17 +0200

    Filled the Container entity with necessary fields.

--- src/main/java/pl/visa/labmanager/container/Container.java
+++ src/main/java/pl/visa/labmanager/container/Container.java
@@ -1,11 +1,30 @@
 package pl.visa.labmanager.container;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pl.visa.labmanager.location.Zone;
+import pl.visa.labmanager.substance.Substance;
 
 @Entity
-@Table(name="container")
+@Getter
+@Setter
+@Table(name="containers")
 public class Container {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
+    @ManyToOne()
+    @JoinColumn(name="substance_id")
+    private Substance substance;
+
+    private String capacity;
+
+    //Miejsce na uwagi dotyczące opakowania, jeżeli jest nietypowe.
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name="zone_id")
+    private Zone zone;
+
 }
