5fae671c27ac9b89eb6697541e806678ff54f720
pszkudla <pszkudla@visa.com>
2026-09-01 10:31:44 +0200

    Added a property photoDir to the Substance entity and changed the szie of the "smiles" field.

--- src/main/java/pl/visa/labmanager/substance/Substance.java
+++ src/main/java/pl/visa/labmanager/substance/Substance.java
@@ -1,25 +1,44 @@
 package pl.visa.labmanager.substance;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.visa.labmanager.LabManagerApplication;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name="substances", uniqueConstraints = {@UniqueConstraint(columnNames = {"iupac_name", "cas_number"})})
+@Table(name = "substances", uniqueConstraints = {@UniqueConstraint(columnNames = {"iupac_name", "cas_number"})})
 public class Substance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min=2, max=2048)
+    @Size(min = 2, max = 2048)
     private String iupacName;
     private String casNumber;
+    @Size(max = 2024)
     private String smiles;
     private String smarts;
     public String uuid;
+
+    @JsonProperty
+    public String photoDir() {
+        Path photoPath = Paths.get(LabManagerApplication.photosPath, uuid + ".png");
+        if (Files.exists(photoPath)) {
+            return photoPath.toString();
+        }
+        else {
+            return null;
+        }
+    }
 }
