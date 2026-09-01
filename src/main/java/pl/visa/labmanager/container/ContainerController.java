29c757ede4ed8af99d50b355dbb5660864e498ea
pszkudla <pszkudla@visa.com>
2026-09-01 14:50:44 +0200

    Created a endpoint showing all DTOs of all cabinets.

--- src/main/java/pl/visa/labmanager/container/ContainerController.java
+++ src/main/java/pl/visa/labmanager/container/ContainerController.java
@@ -1,9 +1,11 @@
 package pl.visa.labmanager.container;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.visa.labmanager.location.zone.ZoneRepository;
 import pl.visa.labmanager.location.zone.ZoneService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/container")
@@ -14,6 +16,12 @@ public class ContainerController {
     public ContainerController(ContainerService containerService, ZoneService zoneService) {
         this.containerService = containerService;
         this.zoneService = zoneService;
+    }
+
+    @GetMapping("/all")
+    public List<ContainerDTO> getAllContainers() {
+        return containerService.getAllContainers().stream()
+                .map(Container::getDto).toList();
     }
 
 
--- src/main/java/pl/visa/labmanager/location/cabinet/CabinetController.java
+++ src/main/java/pl/visa/labmanager/location/cabinet/CabinetController.java
@@ -1,12 +1,10 @@
 package pl.visa.labmanager.location.cabinet;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.visa.labmanager.location.lab.LabService;
 import pl.visa.labmanager.location.lab.Laboratory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,13 +20,17 @@ public class CabinetController {
         this.labService = labService;
     }
 
+    @GetMapping("/all")
+    public List<CabinetDTO> getAllCabinets() {
+        return cabinetService.getAllCabinets().stream()
+                .map(Cabinet::getDTO).toList();
+    }
+
     @PostMapping("/")
     public void addCabinet(@RequestBody Map<String, String> map){
-        System.out.println(map);
         Cabinet addedCabinet = new Cabinet();
         addedCabinet.setCabinetName(map.get("cabinetName"));
         Optional<Laboratory> cabinetLabOpt = labService.getLabFromUuid(UUID.fromString(map.get("labUuid")));
-        System.out.println(labService.getLabFromUuid(UUID.fromString(map.get("labUuid"))));
         if (cabinetLabOpt.isPresent()) {
             Laboratory cabinetLab = cabinetLabOpt.get();
             addedCabinet.setLaboratory(cabinetLab);
