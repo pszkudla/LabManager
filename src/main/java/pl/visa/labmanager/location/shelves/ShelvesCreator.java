45c76b5d13f521bbee80a9e24cb70d94b2481b40
pszkudla <pszkudla@visa.com>
2026-09-01 10:28:54 +0200

    Created an auxiliary class - ShelvesCreator - to create shelves automatically.

---
+++ src/main/java/pl/visa/labmanager/location/shelves/ShelvesCreator.java
@@ -0,0 +1,42 @@
+package pl.visa.labmanager.location.shelves;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.visa.labmanager.location.cabinet.Cabinet;
+import pl.visa.labmanager.location.cabinet.CabinetService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RequestMapping("/createShelves")
+@RestController
+public class ShelvesCreator {
+    private final ShelvesService shelvesService;
+    private final CabinetService cabinetService;
+
+    public ShelvesCreator(ShelvesService shelvesService, CabinetService cabinetService) {
+        this.shelvesService = shelvesService;
+        this.cabinetService = cabinetService;
+    }
+
+    @GetMapping("/allCabinets/{numberOfShelves}")
+    public void createAllCabinets(@PathVariable(name="numberOfShelves") int numberOfShelves) {
+        List<Cabinet> allCabinets = cabinetService.getAllCabinets();
+
+        for (Cabinet cabinet : allCabinets) {
+            for (int i = 1; i<=numberOfShelves; i++) {
+                String nameOfShelf = "Półka numer %s".formatted(i);
+
+                Shelf shelfToAdd = new Shelf();
+                shelfToAdd.setShelfName(nameOfShelf);
+                shelfToAdd.setCabinet(cabinet);
+                shelvesService.addShelf(shelfToAdd);
+            }
+        }
+    }
+
+}
