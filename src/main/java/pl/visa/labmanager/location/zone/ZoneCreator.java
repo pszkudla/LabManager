cac85b425167752371bf252b86cd5a212a3287c8
pszkudla <pszkudla@visa.com>
2026-09-01 12:21:07 +0200

    Created the zones in my database using a separate class - Zone Creator.

---
+++ src/main/java/pl/visa/labmanager/location/zone/ZoneCreator.java
@@ -0,0 +1,45 @@
+package pl.visa.labmanager.location.zone;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.visa.labmanager.location.shelves.Shelf;
+import pl.visa.labmanager.location.shelves.ShelvesRepository;
+import pl.visa.labmanager.location.shelves.ShelvesService;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/zoneCreator")
+public class ZoneCreator {
+    private final ZoneService zoneService;
+    private final ShelvesService shelvesService;
+    private final Random random = new Random();
+    private final String alphabetString = "abcdefghijklmnopqrstuvwxyz";
+
+    public ZoneCreator(ZoneService zoneService, ShelvesService shelvesService) {
+        this.shelvesService = shelvesService;
+        this.zoneService = zoneService;
+    }
+
+    @PostMapping("/createZonesInAllShelves/{min}/{max}")
+    public void createZones(@PathVariable(name = "min") int min, @PathVariable(name = "max") int max) {
+        List<Shelf> allShelves = shelvesService.getAllShelves();
+        for (Shelf shelf : allShelves) {
+            String[] shelfNameParts = shelf.getShelfName().split(" ");
+            String lastShelfNamePart = shelfNameParts[shelfNameParts.length - 1];
+            int numberOfZones = random.nextInt(max - min + 1) + min;
+            for (int i = 1; i <= numberOfZones; i++) {
+                char zoneSymbol = alphabetString.charAt(i-1);
+                String zoneName = "Strefa %s.%s".formatted(lastShelfNamePart, zoneSymbol);
+                Zone zoneToAdd = new Zone();
+                zoneToAdd.setZoneName(zoneName);
+                zoneToAdd.setShelf(shelf);
+                zoneService.addZone(zoneToAdd);
+            }
+        }
+    }
+}
