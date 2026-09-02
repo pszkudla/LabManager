3208b11dfda267e20304077383337b1c92118a47
pszkudla <pszkudla@visa.com>
2026-09-02 14:24:26 +0200

    Separated ZoneDTO intp ZoneDtoIn and ZoneDtoOut.

--- src/main/java/pl/visa/labmanager/location/zone/ZoneDtoIn.java
+++ src/main/java/pl/visa/labmanager/location/zone/ZoneDtoIn.java
@@ -3,16 +3,15 @@ package pl.visa.labmanager.location.zone;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import pl.visa.labmanager.location.shelves.Shelf;
-import pl.visa.labmanager.location.shelves.ShelfDTO;
 
 import java.util.UUID;
 
-@Getter
 @Setter
+@Getter
 @NoArgsConstructor
-public class ZoneDTO {
+public class ZoneDtoIn {
     private String zoneName;
-    private ShelfDTO shelfDto;
-    private String uuid;
+    private String zoneString;
+    private UUID uuid;
+    private UUID shelfUuid;
 }
---
+++ src/main/java/pl/visa/labmanager/location/zone/ZoneDtoOut.java
@@ -0,0 +1,14 @@
+package pl.visa.labmanager.location.zone;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ZoneDtoOut {
+    private String zoneName;
+    private String zoneString;
+    private String uuid;
+}
