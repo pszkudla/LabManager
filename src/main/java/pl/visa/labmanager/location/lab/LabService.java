f73ae210578a9048438ddb75d48bc4317b2cce43
pszkudla <pszkudla@visa.com>
2026-09-02 13:21:59 +0200

    Removed some unnecessary imports from the LabService class.

--- src/main/java/pl/visa/labmanager/location/lab/LabService.java
+++ src/main/java/pl/visa/labmanager/location/lab/LabService.java
@@ -1,10 +1,8 @@
 package pl.visa.labmanager.location.lab;
 
 import org.springframework.stereotype.Service;
-import pl.visa.labmanager.errors.LabNotFoundExcepition;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
