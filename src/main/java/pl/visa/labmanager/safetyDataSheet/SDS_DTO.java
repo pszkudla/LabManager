6404ce7d011b9e4b2bba9ebcaffd1afd7ca1f382
pszkudla <pszkudla@visa.com>
2026-09-07 14:03:15 +0200

    Added "originalFileName" and "uuid" fields to the SDS_DTO.

--- src/main/java/pl/visa/labmanager/safetyDataSheet/SDS_DTO.java
+++ src/main/java/pl/visa/labmanager/safetyDataSheet/SDS_DTO.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @NoArgsConstructor
 @Getter
 @Setter
-public class SDS_DTO_in {
+public class SDS_DTO {
     private String substanceUuid;
     private String language;
     private String supplier;
     private String originalSourceLink;
+    private String originalFileName;
+    private UUID uuid;
 }
