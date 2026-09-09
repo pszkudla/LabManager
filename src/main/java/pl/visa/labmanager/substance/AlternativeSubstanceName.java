c2f4f736ececd9f181e5a9bf15b6f4b917b1852c
pszkudla <pszkudla@visa.com>
2026-09-09 15:33:58 +0200

    Added contraints messages to the AlternativeSubstanceNames.

--- src/main/java/pl/visa/labmanager/substance/AlternativeSubstanceName.java
+++ src/main/java/pl/visa/labmanager/substance/AlternativeSubstanceName.java
@@ -1,7 +1,6 @@
 package pl.visa.labmanager.substance;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +14,11 @@ import java.util.UUID;
 @NoArgsConstructor
 public class AlternativeSubstanceName {
 
-    @Size(min=2, max=2048)
+    @Size(min=2, max=2048, message = "Alternatywna nazwa powinna mieć od 2 do 2048 znaków.")
     @Column(nullable = false)
     private String name;
 
-    @Size(min=1, max=6)
+    @Size(min=1, max=6, message = "Oznaczenie języka powinno mieć od 1 do 6 znaków.")
     private String language;
 
     private UUID uuid = UUID.randomUUID();
