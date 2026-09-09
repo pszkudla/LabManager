33e7698086dfdadd92b5ff1f8d1305ea8505ab97
pszkudla <pszkudla@visa.com>
2026-09-09 14:04:27 +0200

    Changed the source of the data about connection to the database.

--- src/main/java/pl/visa/labmanager/DbUtils/DbUtils.java
+++ src/main/java/pl/visa/labmanager/DbUtils/DbUtils.java
@@ -1,5 +1,6 @@
 package pl.visa.labmanager.DbUtils;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import pl.visa.labmanager.CsvFilesParser.DbLogDetails;
 
 import java.sql.Connection;
@@ -8,6 +9,11 @@ import java.sql.SQLException;
 
 public class DbUtils {
     public static Connection getConnection()  throws SQLException {
-        return DriverManager.getConnection(DbLogDetails.CONNECTION_URL, DbLogDetails.USER_NAME, DbLogDetails.PASSWORD);
+        Dotenv dotenv = Dotenv.configure().load();
+        String url = dotenv.get("spring.datasource.url");
+        String login = dotenv.get("spring.datasource.username");
+        String password = dotenv.get("spring.datasource.password=");
+
+        return DriverManager.getConnection(url, login, password);
     }
 }
