7bd83501e43841829cfc5cd445fd8ac04fb0550d
pszkudla <pszkudla@visa.com>
2026-09-09 13:00:15 +0200

    Created a new Exception and created Exception Handler for it.

---
+++ src/main/java/pl/visa/labmanager/errors/InvalidSubstanceDescriptorException.java
@@ -0,0 +1,7 @@
+package pl.visa.labmanager.errors;
+
+public class InvalidSubstanceDescriptorException extends RuntimeException {
+    public InvalidSubstanceDescriptorException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pl/visa/labmanager/errors/RestControllerAdvice.java
+++ src/main/java/pl/visa/labmanager/errors/RestControllerAdvice.java
@@ -1,11 +1,14 @@
 package pl.visa.labmanager.errors;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.openscience.cdk.exception.CDKException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -19,4 +22,18 @@ public class RestControllerAdvice {
         ErrorResponse error = new ErrorResponse(ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
+
+    @ExceptionHandler(CDKException.class)
+    public ResponseEntity<ErrorResponse> cdkeException(CDKException cdke) {
+        ErrorResponse errorResponse = new ErrorResponse("Błąd CDKException." + cdke.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidSubstanceDescriptorException.class)
+    public ResponseEntity<ErrorResponse> isdeException(InvalidSubstanceDescriptorException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        log.error(ex.getMessage());
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
 }
