e131155c66c6bee9ee60787620b87af4e0c20372
pszkudla <pszkudla@visa.com>
2026-09-14 13:18:09 +0200

    Added function for getting all cabinets from the specified laboratory.

--- src/main/java/pl/visa/labmanager/location/cabinet/CabinetRepository.java
+++ src/main/java/pl/visa/labmanager/location/cabinet/CabinetRepository.java
@@ -2,7 +2,9 @@ package pl.visa.labmanager.location.cabinet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pl.visa.labmanager.location.lab.Laboratory;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,6 +13,7 @@ public interface CabinetRepository extends JpaRepository<Cabinet, Long> {
     @Query("select c FROM Cabinet c where uuid = ?1")
     public Optional<Cabinet> getCabinetByUuid(UUID uuidString);
 
-
+    @Query("select c from Cabinet c WHERE laboratory = ?1")
+    public List<Cabinet> getAllCabinetsInLab(Laboratory laboratory);
 
 }
